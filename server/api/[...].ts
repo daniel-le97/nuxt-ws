f8f1b3db7d47197604c999c57b5aa5ba4e9315e3
daniel-le97 <107774403+daniel-le97@users.noreply.github.com>
2023-12-09 09:15:04 -0700

    webhook testing

---
+++ server/api/[...].ts
@@ -0,0 +1,10 @@
+import { Probot, createNodeMiddleware, createProbot } from "probot";
+import { probot } from "../probot";
+
+const probotApp = new Probot({
+    appId: process.env.PROBOT_APP_ID,
+    privateKey: process.env.PRIVATE_KEY,
+    secret: process.env.WEBHOOK_SECRET,
+});
+
+export default fromNodeMiddleware(createNodeMiddleware(probot, {webhooksPath: "/api/webhooks", probot: probotApp}))
---
+++ server/probot.ts
@@ -0,0 +1,26 @@
+// Deployments API example
+// See: https://developer.github.com/v3/repos/deployments/ to learn more
+
+import { Probot } from "probot";
+
+// For more information, see https://probot.github.io/docs/development/
+export const probot = (app: Probot) => {
+    // Your code here
+    app.log.info("Yay, the app was loaded!");
+    app.on('push',async (context) => {
+        // Creates a deployment on a pull request event
+        // Then sets the deployment status to success
+        // NOTE: this example doesn't actually integrate with a cloud
+        // provider to deploy your app, it just demos the basic API usage.
+        app.log.info(context.payload);
+  
+    
+    // return app;
+  
+    // For more information on building apps:
+    // https://probot.github.io/docs/
+  
+    // To get your app running against GitHub, see:
+    // https://probot.github.io/docs/development/
+    })};
+  
